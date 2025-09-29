e93cac691420ea2d29215cbdd004e9503bc780b2
zoey <z@zoey.ninja>
2025-09-29 15:28:46 +0000

    added configs and formatting

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "corporate"],
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    utils: true,
+  },
+};
+
+export default config;
